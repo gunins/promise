8fe060cc250e7878df243c596a104f63504d6be1
Guntars Simanskis <guntars@diamondgraphics.ie>
2016-04-29 12:26:01 +0100

    Added extra files

---
+++ dev/scripts/i18n/lv-lv/translation.js
@@ -0,0 +1,7 @@
+/**
+ * Created by guntars on 29/04/2016.
+ */
+define({
+    "phrase1": "Sarkans",
+    "phrase2": "Zaļš"
+});
---
+++ dev/scripts/utils/locales.js
@@ -0,0 +1,7 @@
+define(function () {
+    return {
+        en: 'en-gb',
+        lv: 'lv-lv',
+        de: 'de-de'
+    };
+});
